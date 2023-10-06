0ff29e93afe75a01301821de21230816db708e21
technoAl <technoal9@gmail.com>
2023-10-06 12:43:46 -0400

    teleop keyboard adjust

---
+++ src/teleop.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+import roslib
+import rospy
+import math
+import tf
+from geometry_msgs.msg import PoseStamped, Point, Pose, Quaternion, TransformStamped, Vector3, Transform
+from std_msgs.msg import Header
+import termios
+import tty
+import sys
+from tf.transformations import quaternion_from_euler, quaternion_from_matrix
+
+from select import select
+
+def getKey(settings, timeout):
+    tty.setraw(sys.stdin.fileno())
+    # sys.stdin.read() returns a string on Linux
+    rlist, _, _ = select([sys.stdin], [], [], timeout)
+    if rlist:
+        key = sys.stdin.read(1)
+    else:
+        key = ''
+    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+    return key
+
+def saveTerminalSettings():
+    return termios.tcgetattr(sys.stdin)
+
+if __name__ == '__main__':
+    settings = saveTerminalSettings()
+    rospy.init_node('teleop')
+    br = tf.TransformBroadcaster()
+
+    rate = rospy.Rate(10.0)
+    x = 0
+    y = 0
+    z = 0
+    while not rospy.is_shutdown():
+        key = getKey(settings, 0.01)
+        if key == 'w':
+            x += 0.001
+        elif key == 's':
+            x -= 0.001
+        elif key == 'a':
+            y += 0.001
+        elif key == 'd':
+            y -= 0.001
+        elif key == 'q':
+            z += 0.001
+        elif key == 'e':
+            z -= 0.001
+
+        br.sendTransform(
+            (x, y, z), tf.transformations.quaternion_from_euler(0, 0, 0),
+            rospy.Time.now(), "adjust", "world")
+
--- src/unity_publisher.py
+++ src/unity_publisher.py
@@ -17,35 +17,43 @@ if __name__ == '__main__':
 
     rate = rospy.Rate(60.0)
     while not rospy.is_shutdown():
+
+        tag_msg = TransformStamped()
+
+        # Header
+        generic_header = Header()
+        generic_header.stamp = rospy.Time.now()
+        generic_header.frame_id = "world"
+        tag_msg.header = generic_header
+
+        (adjust_trans, adjust_rot) = listener.lookupTransform('/world', '/adjust', rospy.Time(0))
+
         try:
-            tag_msg = TransformStamped()
-
-            # Header
-            generic_header = Header()
-            generic_header.stamp = rospy.Time.now()
-            generic_header.frame_id = "world"
-            tag_msg.header = generic_header
-
-            (cube_trans,cube_rot) = listener.lookupTransform('/world', '/calibration_box_center', rospy.Time(0))
             (camera_trans, camera_rot) = listener.lookupTransform('/world', '/camera', rospy.Time(0))
-            (adjust_trans, adjust_rot) = listener.lookupTransform('/world', '/adjust', rospy.Time(0))
-            (can_trans, can_rot) = listener.lookupTransform('/world', '/corn_can_center', rospy.Time(0))
+            transform.translation = Vector3(camera_trans[0], camera_trans[1], camera_trans[2])
+            transform.rotation = Quaternion(camera_rot[0], camera_rot[1], camera_rot[2], camera_rot[3])
+            tag_msg.transform = transform
+            camera_pub.publish(tag_msg)
+        except:
+            rospy.logwarn("No Camera Transform")
 
+        try:
+            (can_trans, can_rot) = listener.lookupTransform('/world', '/corn_can_center', rospy.Time(0))
+            transform.translation = Vector3(can_trans[0], can_trans[1], can_trans[2])
+            transform.rotation = Quaternion(can_rot[0], can_rot[1], can_rot[2], can_rot[3])
+            tag_msg.transform = transform
+            can_pub.publish(tag_msg)
+        except:
+            rospy.logwarn("No Corn Can Transform")
+
+        try:
+            (cube_trans, cube_rot) = listener.lookupTransform('/world', '/calibration_box_center', rospy.Time(0))
             transform = Transform()
             transform.translation = Vector3(cube_trans[0], cube_trans[1], cube_trans[2])
             transform.rotation = Quaternion(cube_rot[0], cube_rot[1], cube_rot[2], cube_rot[3])
             tag_msg.transform = transform
             cube_pub.publish(tag_msg)
+        except:
+            rospy.logwarn("No Grey Cube")
 
-            transform.translation = Vector3(camera_trans[0], camera_trans[1], camera_trans[2])
-            transform.rotation = Quaternion(camera_rot[0], camera_rot[1], camera_rot[2], camera_rot[3])
-            tag_msg.transform = transform
-            camera_pub.publish(tag_msg)
 
-            transform.translation = Vector3(can_trans[0], can_trans[1], can_trans[2])
-            transform.rotation = Quaternion(can_rot[0], can_rot[1], can_rot[2], can_rot[3])
-            tag_msg.transform = transform
-            can_pub.publish(tag_msg)
-
-        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
-            continue
